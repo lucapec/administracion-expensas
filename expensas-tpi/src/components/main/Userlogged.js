6ae64656f5ac9bd35d22626fbc19773e370a373a
lucapec <lucapecorelli1@gmail.com>
2022-07-20 19:14:57 -0300

    Chi cheñolll

--- expensas-tpi/src/components/main/Userlogged.js
+++ expensas-tpi/src/components/main/Userlogged.js
@@ -1,5 +1,7 @@
 import { useAuthDispatch, useAuth } from "../context/AuthContextProvider";
 import React, { useEffect, useState } from "react";
+import Table from 'react-bootstrap/Table';
+import {Modal, ModalBody, ModalHeader, ModalFooter} from 'reactstrap';
 import "./UserLogged.css";
 
 function Userlogged() {
@@ -18,6 +20,69 @@ function Userlogged() {
     })}, 
     []);
 
+    const [modalEditar, setModalEditar] = useState(false);
+    const [modalEliminar, setModalEliminar] = useState(false);
+    const [modalInsertar, setModalInsertar] = useState(false);
+
+    const [userSelected, setUserSelected] = useState({
+      sudo: '',
+      id: '',
+      name: '',
+      lastname: '',
+      password: '',
+      edificio: '',
+      valor: '',
+      mes: '',
+    });
+
+    const selectUser=(element, _case)=>{
+      setUserSelected(element);
+      (_case==='Editar')?setModalEditar(true):setModalEliminar(true)
+        };
+
+        const handleChange=e=>{
+          const {name, value}=e.target;
+          setUserSelected((prevState)=>({
+            ...prevState,
+            [name]: value
+          }));
+        }
+
+        const edit=()=>{
+          var newData=users;
+          newData.map(users=>{
+            if(users.id===userSelected.id){
+              users.sudo=userSelected.sudo;
+              users.name=userSelected.name;
+              users.lastname=userSelected.lastname;
+              users.password=userSelected.password;
+              users.edificio=userSelected.edificio;    
+              users.valor=userSelected.valor;
+              users.mes=userSelected.mes;
+            }
+          });
+          setUsers(newData);
+          setModalEditar(false);
+        }
+      
+        const eliminar =()=>{
+          setUsers(users.filter(users=>users.id!==userSelected.id));
+          setModalEliminar(false);
+        }
+
+        const abrirModalInsertar=()=>{
+          setUserSelected(null);
+          setModalInsertar(true);
+        }
+
+        const insertar =()=>{
+          var newValue=userSelected;
+          newValue.id=users[users.length-1].id+1;
+          var newData = users;
+          newData.push(newValue);
+          setUsers(newData);
+          setModalInsertar(false);
+        }
 
   return (
     <div className="userloged">
@@ -43,17 +108,290 @@ function Userlogged() {
       )}
       {auth.currentUser.sudo == 'admin' && (
         <div>
+           <button className="btn btn-success" onClick={()=>abrirModalInsertar()}>Insertar</button>
+             <div>
+                <Table striped bordered hover variant="dark">
+                <thead>
+                <tr>
+          <th>Id</th>
+          <th>Nombre</th>
+          <th>Apellido</th>
+          <th colSpan={2}>Deuda</th>
+        </tr>
+      </thead>
           {users.map(x => (
-            <div>
-              {x.name}
-            </div>
-          ))}
+           
+       
+      <tbody>
+        <tr>
+          <td>{x.sudo}</td>
+          <td>{x.id}</td>
+          <td>{x.name}</td>
+          <td>{x.lastname}</td>
+          <td>{x.edificio}</td>
+          <td>{x.valor}</td>
+          <td>{x.mes}</td>
+          <td><button className="btn btn-primary" onClick={()=>selectUser(x, 'Editar')}>Editar</button> {"   "} 
+              <button className="btn btn-danger" onClick={()=>selectUser(x, 'Eliminar')}>Eliminar</button></td>
+        </tr>
+        </tbody>
+  ))}
+          
+    </Table>
+    <Modal isOpen={modalEditar}>
+        <ModalHeader>
+          <div>
+            <h3>Editar Usuario</h3>
+          </div>
+        </ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label>Sudo</label>
+            <input
+              className="form-control"
+              readOnly
+              type="text"
+              name="sudo"
+              value={userSelected && userSelected.sudo}
+            />
+            <br />
+
+            <label>Id</label>
+            <input
+              className="form-control"
+              type="text"
+              name="id"
+              value={userSelected && userSelected.id}
+              onChange={handleChange}
+            />
+            <br />
+
+             <br />
+
+            <label>Nombre</label>
+            <input
+              className="form-control"
+              type="text"
+              name="name"
+              value={userSelected && userSelected.name}
+              onChange={handleChange}
+            />
+            <br />
+            <br />
+
+<label>Apellido</label>
+<input
+  className="form-control"
+  type="text"
+  name="lastname"
+  value={userSelected && userSelected.lastname}
+  onChange={handleChange}
+/>
+<br />
+<br />
+
+<label>Contraseña</label>
+<input
+  className="form-control"
+  type="text"
+  name="password"
+  value={userSelected && userSelected.password}
+  onChange={handleChange}
+/>
+<br />
+<br />
+
+<label>Edificio</label>
+<input
+  className="form-control"
+  type="text"
+  name="edificio"
+  value={userSelected && userSelected.edificio}
+  onChange={handleChange}
+/>
+<br />
+<br />
+
+<label>Deuda</label>
+<input
+  className="form-control"
+  type="text"
+  name="valor"
+  value={userSelected && userSelected.valor}
+  onChange={handleChange}
+/>
+<br />
+<br />
+
+<label>mes</label>
+<input
+  className="form-control"
+  type="text"
+  name="mes"
+  value={userSelected && userSelected.mes}
+  onChange={handleChange}
+/>
+<br />
+    </div>
+    </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-primary" onClick={()=>edit()}>
+            Actualizar
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={()=>setModalEditar(false)}
+          >
+            Cancelar
+          </button>
+        </ModalFooter>
+      </Modal>
+
+      <Modal isOpen={modalEliminar}>
+        <ModalBody>
+          Estás Seguro que deseas eliminar el Usuario {userSelected && userSelected.nombre}
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-danger" onClick={()=>eliminar()}>
+            Sí
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={()=>setModalEliminar(false)}
+          >
+            No
+          </button>
+        </ModalFooter>
+      </Modal>
+
+      <Modal isOpen={modalInsertar}>
+        <ModalHeader>
+          <div>
+            <h3>Insertar País</h3>
+          </div>
+        </ModalHeader>
+        <ModalBody>
+          <div className="form-group">
+            <label>ID</label>
+            <input
+              className="form-control"
+              readOnly
+              type="text"
+              name="id"
+              value={users.id + 1 }
+            />
+            <br />
+
+            <label>Sudo</label>
+            <input
+              className="form-control"
+              readOnly
+              type="text"
+              name="sudo"
+              value={userSelected ? userSelected.sudo: ''}
+              onChange={handleChange}
+            />
+            <br />
+
+            <label>Id</label>
+            <input
+              className="form-control"
+              type="text"
+              name="id"
+              value={userSelected ? userSelected.id: ''}
+              onChange={handleChange}
+            />
+            <br />
+
+             <br />
+
+            <label>Nombre</label>
+            <input
+              className="form-control"
+              type="text"
+              name="name"
+              value={userSelected ? userSelected.name: ''}
+              onChange={handleChange}
+            />
+            <br />
+            <br />
+
+<label>Apellido</label>
+<input
+  className="form-control"
+  type="text"
+  name="lastname"
+  value={userSelected ? userSelected.lastname: ''}
+  onChange={handleChange}
+/>
+<br />
+<br />
+
+<label>Contraseña</label>
+<input
+  className="form-control"
+  type="text"
+  name="password"
+  value={userSelected ? userSelected.password: ''}
+  onChange={handleChange}
+/>
+<br />
+<br />
+
+<label>Edificio</label>
+<input
+  className="form-control"
+  type="text"
+  name="edificio"
+  value={userSelected ? userSelected.edificio: ''}
+  onChange={handleChange}
+/>
+<br />
+<br />
+
+<label>Deuda</label>
+<input
+  className="form-control"
+  type="text"
+  name="valor"
+  value={userSelected ? userSelected.valor: ''}
+  onChange={handleChange}
+/>
+<br />
+<br />
+
+<label>Mes</label>
+<input
+  className="form-control"
+  type="text"
+  name="mes"
+  value={userSelected ? userSelected.mes: ''}
+  onChange={handleChange}
+/>
+<br />
         </div>
-  )}
+        </ModalBody>
+        <ModalFooter>
+          <button className="btn btn-primary"
+          onClick={()=>insertar()}>
+            Insertar
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={()=>setModalInsertar(false)}
+          >
+            Cancelar
+          </button>
+        </ModalFooter>
+      </Modal>
+    </div>
+  
 
   </div>
+)}</div>
   )
       }
+
     
 
 export default Userlogged;
--- expensas-tpi/src/index.js
+++ expensas-tpi/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './index.css';
 import App from './App';
@@ -9,8 +10,7 @@ root.render(
   
            <App />
 
-    
-  
+
 );
 
 
